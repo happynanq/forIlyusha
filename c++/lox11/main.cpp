37ff9aa056302a258d19f90885dc0f818c186cb4
Kirill <54491606+happynanq@users.noreply.github.com>
2021-12-07 20:08:19 +0700

    Add files via upload

---
+++ c++/lox11/main.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+//Голястиков  Илюша, Марьясов Влад
+using namespace std;
+
+void first()
+{
+    int a[10], b[10] = {0}, sum;
+    for(int i = 0; i <= 9; i++ )
+    {
+        a[i] = rand() % 10;
+
+        sum +=a[i];
+
+        b[i] = sum;
+        cout << "a[i] = " << a[i] << endl;
+        cout << "b[i] = " << b[i] << endl;
+    }
+}
+void second()
+{
+    int a[10];
+    for(int i = 0; i <= 9; i++ )
+    {
+        a[i] = i;
+    }
+    for(int i = 9; i >= 0; i--)
+    {
+        cout << a[i];
+    }
+}
+void third()
+{
+    int a[10];
+    for(int i = 0; i <= 9; i++ )
+    {
+        a[i] = i;
+    }
+    for(int i = 0; i <= 4; i++)
+    {
+        swap(a[i], a[10-i]);
+    }
+    for(int i = 9; i >= 0; i--)
+    {
+        cout << a[i];
+    }
+}
+void fourth()
+{
+    const int size = 10;
+    int a[size];
+
+    for(int i = 0; i < size; i++ )
+    {
+        a[i] = i;
+    }
+
+    const int temp = a[size-1];
+
+    for(int i = size; i >= 1; i--)
+        a[i] = a[i-1];
+
+    a[0] = temp;
+    for(int i = 0; i < size; i++)
+    {
+        cout << a[i];
+    }
+}
+void fifth()
+{
+    int temp, step = 5;
+    const int size = 10;
+    int array[size];
+
+
+
+    for(int i = 0; i < size; i++ )
+    {
+        array[i] = i;
+    }
+
+    for (int i = 0, j; i < step; ++i)
+    {
+        temp = array[0];
+        for (j = 0; j < size - 1; ++j)
+            array[j] = array[j + 1];
+        array[j] = temp;
+    }
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i];
+    }
+}
+void sixth()
+{
+    const int N = 10;
+    int a[N];
+    for(int i = 0; i <= N; i++ )
+    {
+        if(i%2 == 0 and N /2 < i )
+        {
+            a[i] = 0;
+        }else if(i%2!=0)
+        {
+            a[i] = i;
+        }else
+        {
+            a[i] = 0;
+        }
+
+    }
+    for(int i = 0; i < N; i++)
+    {
+        cout << a[i];
+    }
+
+}
+
+
+int main()
+{
+    fourth();
+    return 0;
+}
---
+++ c++/lox4/main.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+//Голястиков  Илюша, Марьясов Влад
+using namespace std;
+
+void first()
+{
+    int d1,d2,m1,m2,y1,y2;
+    cin >> d1 >> m1 >> y1 >> d2 >> m2 >> y2;
+    if(y1 < y2)
+    {
+        cout<<"ïåðâàÿ äàòà ïðåäøåñòâóåò âòîðîé";
+    }else if(y1 > y2)
+    {
+        cout<<"ïåðâàÿ äàòà ñëåäóåò ïîñëå âòîðîé";
+    }else
+    {
+        if(m1<m2)
+        {
+            cout<<"ïåðâàÿ äàòà ïðåäøåñòâóåò âòîðîé";
+        }else if(m1 > m2)
+        {
+            cout<<"ïåðâàÿ äàòà ñëåäóåò ïîñëå âòîðîé";
+        }else
+        {
+            if(d1<d2)
+            {
+                cout<<"ïåðâàÿ äàòà ïðåäøåñòâóåò âòîðîé";
+            }else if(d1>d2)
+            {
+                cout<<"ïåðâàÿ äàòà ñëåäóåò ïîñëå âòîðîé";
+            }
+            else
+            {
+                cout<<"Äàòû ñîâïàäàþò";
+            }
+        }
+    }
+
+
+}
+
+void second()
+{
+    int d, m, y;
+    int dc = 24, mc = 11, yc = 2021;
+
+    cin >> d >> m >> y;
+    int age = 0, eage = 0;
+    age += yc - y;
+    if(mc < m)
+    {
+        age -=1;
+    }else if(mc == m)
+    {
+        if(d > dc)
+        {
+            age -=1;
+        }
+    }
+    eage = age%10;
+    if(age  > 10 and age < 20)
+    {
+        cout << age << " ëåò";
+    }else if(eage == 1)
+    {
+        cout << age <<" ãîä"<<endl;
+    }else if(eage == 2 or eage == 4 or eage == 3)
+    {
+        cout << age << " ãîäà"<<endl;
+    }else
+    {
+        cout << age << " ëåò";
+    }
+}
+
+void third()
+{
+    double a, b, c, y = 0;
+    cout << "Введите аргументы" << endl;
+    cin >> a >> b >> c;
+    if( a== 0 and b == 0 and c == 0)
+    {
+        cout <<"уравнение имеет бесконечное число корней";
+    }else if(c !=0 and a == 0 and b == 0)
+    {
+        cout << "корней нет";
+    }else
+    {
+        double d = b*b - 4*a*c;
+        if(d < 0)
+        {
+            cout<< "нет действительных корней";
+
+        }else if(d == 0)
+        {
+            cout << "Один корень: " << -(b/2*a);
+        }else
+        {
+            cout << "Два корня: " << ((-b-sqrt(d))/2*a) << " and " << ((-b+sqrt(d))/2*a);
+        }
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    third();
+    return 0;
+}
